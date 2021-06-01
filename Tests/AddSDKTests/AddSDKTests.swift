404792e2f7784e5ca84571bac86f510cc89c4d3b
Sergio Peñín <spenin@hiberus.com>
2021-06-01 16:00:35 +0200

    Changes for SPM

---
+++ Tests/AddSDKTests/AddSDKTests.swift
@@ -0,0 +1,11 @@
+    import XCTest
+    @testable import AddSDK
+
+    final class AddSDKTests: XCTestCase {
+        func testExample() {
+            // This is an example of a functional test case.
+            // Use XCTAssert and related functions to verify your tests produce the correct
+            // results.
+            XCTAssertEqual(AddSDK().text, "Hello, World!")
+        }
+    }
